b00b346b4e6bef9b3ad4fa9a8374957160bf8315
Matthew Desramault <matthew.desramault@student.vives.be>
2022-10-19 15:48:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/teststuff/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.teststuff
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var dateEdt: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dateEdt = findViewById(R.id.idEdtDate)
+
+        dateEdt.setOnClickListener {
+            // on below line we are getting
+            // the instance of our calendar.
+            val c = Calendar.getInstance()
+
+            // on below line we are getting
+            // our day, month and year.
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            // on below line we are creating a
+            // variable for date picker dialog.
+            val datePickerDialog = DatePickerDialog(
+                // on below line we are passing context.
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // on below line we are setting
+                    // date to our edit text.
+                    val dat = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                    dateEdt.setText(dat)
+                },
+                // on below line we are passing year, month
+                // and day for the selected date in our date picker.
+                year,
+                month,
+                day
+            )
+            // at last we are calling show
+            // to display our date picker dialog.
+            datePickerDialog.show()
+
+        }
+
+    }
+
+
+}
